93d69c2ec011aa685c4f7b9b92fc75a26883e06f
LuzAlvarez713 <125053388@upq.edu.mx>
2026-09-17 17:57:23 -0600

    IntroJS
    Se realizaron varios archivos dentro de esta carpeta con varios procesos dentro de ellos

---
+++ introJS/introNode.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo NODE")
+let edad1=20;
+let edad2=19
+console.log("Edad Promedio: ");
+console.log((edad1+edad2)/2);
+
+console.log("Medidor de Procesos");
+
+console.time('miProceso');
+    for(let i=0; i<1000000000; i++){}
+console.timeEnd('miProceso');
+
+
